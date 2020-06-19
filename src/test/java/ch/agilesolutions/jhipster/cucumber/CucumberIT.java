e1f5998a084f5ae1196918e9a5036a1d353a4821
robert.rong <robert.rong@juliusbaer.com>
2020-06-19 11:45:56 +0200

    add formatting plugins

--- src/test/java/ch/agilesolutions/jhipster/cucumber/CucumberIT.java
+++ src/test/java/ch/agilesolutions/jhipster/cucumber/CucumberIT.java
@@ -2,11 +2,12 @@ package ch.agilesolutions.jhipster.cucumber;
 
 import org.junit.runner.RunWith;
 
-import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = "pretty", features = "src/test/features")
+@CucumberOptions(features = {"src/test/features"}, plugin = {"pretty", "html:target/reports/cucumber/html",
+        "json:target/cucumber.json", "usage:target/usage.jsonx", "junit:target/junit.xml"})
 
 public class CucumberIT  {
 
